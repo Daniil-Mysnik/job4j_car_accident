f121b0e0a900626b4bc270025387f5dccd7839fc
daniil_mysnik_990 <69dantes6901>
2021-02-26 16:57:50 +0600

    [#317801]

--- src/main/java/ru.job4j.accident/control/IndexControl.java
+++ src/main/java/ru.job4j.accident/control/IndexControl.java
@@ -4,10 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model) {
+        List<String> data = List.of("aaa", "bbb", "ccc");
+        model.addAttribute("lst", data);
         return "index";
     }
 }
